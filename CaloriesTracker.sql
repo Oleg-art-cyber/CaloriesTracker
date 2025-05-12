bb887da4b541aa43de315623b42301537a5d7f18
Oleg Kustov <olegperm@icloud.com>
2025-05-12 13:34:22 +0300

    Add files via upload

--- CaloriesTracker.sql
+++ CaloriesTracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 11, 2025 at 02:47 PM
+-- Generation Time: May 12, 2025 at 12:33 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,6 +42,49 @@ CREATE TABLE `AchievementProgress` (
   `achievement_id` int(11) DEFAULT NULL,
   `user_progress_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` tinyint(3) UNSIGNED NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `label` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `label`) VALUES
+(1, 'vegetable', 'Vegetables'),
+(2, 'fruit', 'Fruits'),
+(3, 'meat', 'Meat'),
+(4, 'poultry', 'Poultry'),
+(5, 'fish', 'Fish'),
+(6, 'grain', 'Grains & Cereals'),
+(7, 'dairy', 'Dairy'),
+(8, 'prepared', 'Prepared meal'),
+(9, 'sweets', 'Sweets'),
+(10, 'other', 'Other'),
+(11, 'root_veg', 'Root Vegetables'),
+(12, 'leafy_veg', 'Leafy Greens'),
+(13, 'berry', 'Berries'),
+(14, 'bakery', 'Bakery'),
+(15, 'processed_meat', 'Processed Meat'),
+(16, 'seafood', 'Seafood'),
+(17, 'fermented_dairy', 'Fermented Dairy'),
+(18, 'nuts_seeds', 'Nuts & Seeds'),
+(19, 'oil_fat', 'Oils & Fats'),
+(20, 'snack_fastfood', 'Snacks & Fast-food'),
+(21, 'beverage', 'Beverages'),
+(22, 'alcohol', 'Alcohol'),
+(23, 'condiment', 'Condiments / Sauces'),
+(24, 'soup_stew', 'Soups & Stews'),
+(25, 'protein_product', 'Protein Products');
 
 -- --------------------------------------------------------
 
@@ -86,26 +129,31 @@ CREATE TABLE `PhysicalActivity` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Product`
+-- Table structure for table `product`
 --
 
-CREATE TABLE `Product` (
+CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `calories` int(11) DEFAULT NULL,
   `fat` float DEFAULT NULL,
   `protein` float DEFAULT NULL,
   `carbs` float DEFAULT NULL,
-  `category` varchar(50) DEFAULT NULL
+  `category_id` tinyint(3) UNSIGNED NOT NULL DEFAULT 10,
+  `created_by` int(11) DEFAULT NULL,
+  `is_public` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `Product`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `Product` (`id`, `name`, `calories`, `fat`, `protein`, `carbs`, `category`) VALUES
-(2, 'Cottage Cheese 5% (Tnuva)', 98, 4.5, 12, 3, 'Milk'),
-(3, 'Cottage Cheese 3% (Tnuva)', 98, 3, 11, 3, 'Dairy');
+INSERT INTO `product` (`id`, `name`, `calories`, `fat`, `protein`, `carbs`, `category_id`, `created_by`, `is_public`) VALUES
+(4, 'Tnuva Choco Bag', 65, 2, 3, 10, 7, NULL, 1),
+(5, 'Test food', 100, 1, 5, 10, 3, NULL, 1),
+(6, 'Test food 2', 65, 23, 2, 2, 18, NULL, 1),
+(7, 'Test food', 213, 23, 21, 213, 10, 2, 1),
+(8, 'Альма тест ', 123, 3, 2, 4, 10, 3, 0);
 
 -- --------------------------------------------------------
 
@@ -132,7 +180,8 @@ CREATE TABLE `User` (
 
 INSERT INTO `User` (`id`, `name`, `email`, `password`, `weight`, `height`, `age`, `goal`, `role`, `created_at`) VALUES
 (1, 'Oliver', 'oleg@example.com', '$2b$10$XwpwQZxljmNbxefMIoklzuuofrGmDHci29cojcVf2d2D2Eu2dyz6O', 74, 175, 28, 'lose', 'user', '2025-05-11 12:08:36'),
-(2, 'Алла', 'allasuper@example.com', '$2b$10$/g8IDSay4U8eH.7iQhlAsu4pcn0H1huJjq9vxBXnOIRydtbxIbO6O', 50, 155, 22, 'lose', 'admin', '2025-05-11 12:26:23');
+(2, 'Алла', 'allasuper@example.com', '$2b$10$/g8IDSay4U8eH.7iQhlAsu4pcn0H1huJjq9vxBXnOIRydtbxIbO6O', 50, 155, 22, 'lose', 'admin', '2025-05-11 12:26:23'),
+(3, 'Alma', 'alma@example.com', '$2b$10$2K3dEi/lcBke9wEnJaTqz.8gsIQh4naIQW4Uo/gABTchHjpzz2OXu', 99, 179, 49, 'lose', 'user', '2025-05-12 10:17:13');
 
 -- --------------------------------------------------------
 
@@ -168,6 +217,13 @@ ALTER TABLE `AchievementProgress`
   ADD KEY `user_progress_id` (`user_progress_id`);
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
 -- Indexes for table `Meal`
 --
 ALTER TABLE `Meal`
@@ -189,10 +245,11 @@ ALTER TABLE `PhysicalActivity`
   ADD KEY `user_progress_id` (`user_progress_id`);
 
 --
--- Indexes for table `Product`
+-- Indexes for table `product`
 --
-ALTER TABLE `Product`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product_cat` (`category_id`);
 
 --
 -- Indexes for table `User`
@@ -218,6 +275,12 @@ ALTER TABLE `Achievement`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
 -- AUTO_INCREMENT for table `Meal`
 --
 ALTER TABLE `Meal`
@@ -230,16 +293,16 @@ ALTER TABLE `PhysicalActivity`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Product`
+-- AUTO_INCREMENT for table `product`
 --
-ALTER TABLE `Product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `UserProgress`
@@ -276,6 +339,12 @@ ALTER TABLE `MealProduct`
 --
 ALTER TABLE `PhysicalActivity`
   ADD CONSTRAINT `physicalactivity_ibfk_1` FOREIGN KEY (`user_progress_id`) REFERENCES `UserProgress` (`id`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `fk_product_cat` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 --
 -- Constraints for table `UserProgress`
